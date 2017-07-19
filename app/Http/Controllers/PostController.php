8c555c833ce2712e61bffb301a60b7d8a91d0c74
vagrant <vagrant@example>
2017-07-19 17:09:38 +0000

    We create our applicationcontroller

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;// We use it to import post model
+use Illuminate\Http\Request;
+
+use  App\Http\Requests;
+use Illuminate\Session\Store;// Here we use this namespade to DI and resolve this service from laravel container
+
+class PostController extends Controller
+{
+    public function getIndex(Store $session) {
+      $post = new Post();
+      $posts = $post->getPosts($session);// Here we call our Model object method to check if we store data in session and retrive that data and save it in the variable
+      return view('blog.index', ['posts' => $posts]);// here we retrun View response object and pased retrived data to the our view
+    }
+}
