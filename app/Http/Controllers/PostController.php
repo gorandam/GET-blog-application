0a6f03a8c6b5cc6168f28f2b64bc9c83c1dff6a3
vagrant <vagrant@example>
2017-07-24 15:32:14 +0000

    We finish our controllers and models

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -30,7 +30,7 @@ class PostController extends Controller
       $posts = $post->getPost($session, $id);// Here we call our Model object method to retrieve single post and save it to the variable
       return view('admin.index', ['posts' => $posts]);// here we retrun View response object and pased retrived data to the our view
     }
-
+     // Here are ADMIN methods
     // This is the method that allows admin to create new post
     public function getAdminCreate(Store $session, $id) {
       return view('admin.create'); // This returns admin create template to create posts by admin
@@ -45,13 +45,21 @@ class PostController extends Controller
 
     // This is the method triggered when user (admin) submits our admin.create
     public function postAdminCreate(Store $session, Request $request) {
+      $this->validate($request, [ // Here we place our validation logic
+        'title' => 'required|min:5',
+        'content' => 'required|min:10'
+      ]);
       $post = new Post();
       $post = $post->addPost($session, $request->input('title'), $request->input('content')); // Here we use $request->input() method to retrieve users inpit from request
-      return redirect()->route('admin.index')->with('info', 'Post created, Title is: ' . $request->input('title'));
+      return redirect()->route('admin.index')->with('info', 'Post created, Title is: ' . $request->input('title'));// Here we return Redirect HTTP header object with specified url
     }
 
     //This is the method triggered when user (admin) submits admin.edit form
     public function postAdminUpdate(Store $session, Request $request) {
+      $this->validate($request, [ // Here we place our validation logic
+        'title' => 'required|min:5',
+        'content' => 'required|min:10'
+      ]);
       $post = new Post();
       $post = $post->editPost($session, $request->input('id'), $request->input('title'), $request->input('content')); // Here we use $request->input() method to retrieve users inpit from request
       return redirect()->route('admin.index')->with('info', 'Post edited, new Title is: ' . $request->input('title'));
--- app/Post.php
+++ app/Post.php
@@ -1,4 +1,3 @@
-u
 <?php
 
 namespace App;
